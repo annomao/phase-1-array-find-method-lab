62523d3852b9c979e8d6560b6e98d9bf3d87bd10
annomao <omaoannkerubo@gmail.com>
2022-04-10 00:03:45 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+  const recordResult = record.find(recordObj=> recordObj.result == "W")
+  if (recordResult === undefined){
+    //return nothing which is equivalent to undefined
+    //find a better solution
+  }
+  else{
+    return recordResult.year;
+  }
+}
